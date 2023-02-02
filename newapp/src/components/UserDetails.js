eec14702a2e51aa767fd6973cf230e0635bef51d
Mohit kumar <mohitkvk455@gmail.com>
2023-02-03 00:24:23 +0530

    Event handling video 11

--- newapp/src/components/UserDetails.js
+++ newapp/src/components/UserDetails.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const UserDetails =({ user = {} })=>{
-    return (
-        <div>
-            <h3>{user.name}</h3>
-            <h5>
-                Email: {user.email}<br/>
-                Phone number: {user.phoneNumber}
-            </h5>
-        </div>
-    );
+const UserDetails = ({ user = {} }) => {
+  const [h1Text, setH1Text] = useState("This is Demo component");
+  return (
+    <div>
+      <h3>{user.name}</h3>
+      <h5>
+        Email: {user.email}
+        <br />
+        Phone number: {user.phoneNumber}
+      </h5>
+      <h1>{h1Text}</h1>
+      <button
+        onClick={(e) => {
+          setH1Text("This is Cipher Schools")
+        }}
+      >
+        Click Me!
+      </button>
+    </div>
+  );
 };
 
-export default UserDetails
+export default UserDetails;
